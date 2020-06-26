5e2826bc6bd9b295356efe95f2c0d5807e6e581a
almeida_gabriel <almeida_gabriel@sicredi.com.br>
2020-06-26 20:31:22 -0300

    valida a imagem secreta de entrada e converte as dimensoes para binario

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ typedef struct {
 
 // Protótipos
 void load(char* name, Img* pic);
+int* decToBin(int dec);
 
 // Carrega uma imagem para a struct Img
 void load(char* name, Img* pic)
@@ -32,6 +33,31 @@ void load(char* name, Img* pic)
     printf("Load: %s (%d x %d x %d)\n", name, pic->width, pic->height, chan);
 }
 
+int* decToBin(int dec) {
+    if( dec > 4095) {
+        printf("Valor invalido para conversao, o valor maximo eh 4095");
+        exit(1);
+    }
+    int i = 0;
+    int binValue[12];
+    while (dec > 0) {
+
+        binValue[i] = dec % 2;
+        dec = dec / 2;
+        i++;
+    }
+
+//    printf("\n[");
+//    for (int k = i - 1; k >= 0; k--){
+//
+//        printf("%d", binValue[k]);
+//
+//    }
+//    printf("]\n");
+
+    return *binValue;
+}
+
 int main(int argc, char** argv)
 {
     Img base, secreta;
@@ -39,18 +65,36 @@ int main(int argc, char** argv)
         printf("loader [img base] [img secreta]\n");
         exit(1);
     }
+
     load(argv[1], &base);
     load(argv[2], &secreta);
 
-    printf("Primeiros 10 pixels da imagem base:\n");
-    for(int i=0; i<10; i++)
-        printf("[%02X %02X %02X] ", base.img[i].r, base.img[i].g, base.img[i].b);
-    printf("\n\n");
+    //start codigo
 
-    printf("Primeiros 10 pixels da imagem secreta:\n");
-    for(int i=0; i<10; i++)
-        printf("[%02X %02X %02X] ", secreta.img[i].r, secreta.img[i].g, secreta.img[i].b);
-    printf("\n\n");
+    int areaBase = base.height * base.width;
+    int areaSecreta = secreta.height * secreta.width;
+
+    if(areaBase/4+24 < areaSecreta) {
+        printf("Imagem invalida, a imagem secreta deve ter no maximo 1/4 do tamanho da imagem base");
+        exit(1);
+    }
+
+    int binWidth = decToBin(secreta.width);
+    int binHeight = decToBin(secreta.height);
+
+    printf("largura %d, altura %d\n", base.width, base.height);
+
+    //end codigo
+
+//    printf("Primeiros 10 pixels da imagem base:\n");
+//    for(int i=0; i<10; i++)
+//        printf("[%02X %02X %02X] ", base.img[i].r, base.img[i].g, base.img[i].b);
+//    printf("\n\n");
+//
+//    printf("Primeiros 10 pixels da imagem secreta:\n");
+//    for(int i=0; i<10; i++)
+//        printf("[%02X %02X %02X] ", secreta.img[i].r, secreta.img[i].g, secreta.img[i].b);
+//    printf("\n\n");
 
     printf("Teste: gravando imagem base em saida.bmp\n");
     SOIL_save_image("saida.bmp", SOIL_SAVE_TYPE_BMP,
