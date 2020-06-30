b66ef83dbef332131257672996f27695949e78c5
vbazanella <vinicius.bazanella1@gmail.com>
2020-06-30 19:12:20 -0300

    Solves the decToBin issue

--- main.c
+++ main.c
@@ -18,7 +18,9 @@ typedef struct {
 
 // Protótipos
 void load(char* name, Img* pic);
-int* decToBin(int dec, int tam);
+int* decToBin8(int dec);
+int* decToBin12(int dec);
+int binToDec(int *bin, int tam);
 
 // Carrega uma imagem para a struct Img
 void load(char* name, Img* pic)
@@ -33,13 +35,13 @@ void load(char* name, Img* pic)
     printf("Load: %s (%d x %d x %d)\n", name, pic->width, pic->height, chan);
 }
 
-int* decToBin(int dec, int tam) {
+int* decToBin12(int dec) {
     if( dec > 4095) {
         printf("Valor invalido para conversao, o valor maximo eh 4095");
         exit(1);
     }
     int i = 0;
-    int binValue[tam];
+    int binValue[12];
     while (dec > 0) {
 
         binValue[i] = dec % 2;
@@ -47,24 +49,67 @@ int* decToBin(int dec, int tam) {
         i++;
     }
 
-    int binReverse[tam];
+    static int binReverse[12];
 
-    for(int j = i; j<tam; j++){
+    for(int j = i; j<12; j++){
         binValue[j] = 0;
     }
 
-    for(int j = 0; j<tam; j++) {
-        int k = tam-1-j;
+    for(int j = 0; j<12; j++) {
+        int k = 12-1-j;
         binReverse[j] = binValue[k];
     }
 
-    printf("\n[");
-    for(int j = 0; j<tam; j++) {
-        printf("%d", binReverse[j]);
-    }
-    printf("]\n");
+//    printf("\n[");
+//    for(int j = 0; j<12; j++) {
+//        printf("%d", binReverse[j]);
+//    }
+//    printf("]\n");
 
-    return *binReverse;
+    return binReverse;
+}
+
+int* decToBin8(int dec) {
+    if( dec > 4095) {
+        printf("Valor invalido para conversao, o valor maximo eh 4095");
+        exit(1);
+    }
+    int i = 0;
+    int binValue[8];
+    while (dec > 0) {
+
+        binValue[i] = dec % 2;
+        dec = dec / 2;
+        i++;
+    }
+
+    static int binReverse[8];
+
+    for(int j = i; j<8; j++){
+        binValue[j] = 0;
+    }
+
+    for(int j = 0; j<8; j++) {
+        int k = 8-1-j;
+        binReverse[j] = binValue[k];
+    }
+
+//    printf("\n[");
+//    for(int j = 0; j<8; j++) {
+//        printf("%d", binReverse[j]);
+//    }
+//    printf("]\n");
+
+    return binReverse;
+}
+
+int binToDec(int *bin, int tam){
+    int i, n, sum = 0;
+    for (i = 0; i < tam; i++) {
+        n = *(bin + i) - '0';
+        sum += (n * (1 << (tam - (i + 1))));
+    }
+    return sum;
 }
 
 int main(int argc, char** argv)
@@ -83,37 +128,60 @@ int main(int argc, char** argv)
     int areaBase = base.height * base.width;
     int areaSecreta = secreta.height * secreta.width;
 
-    if(areaBase/4+24 < areaSecreta) {
+    if(areaBase/4 < areaSecreta+24) {
         printf("Imagem invalida, a imagem secreta deve ter no maximo 1/4 do tamanho da imagem base");
         exit(1);
     }
 
     printf("\nlargura: ");
-    int binWidth = decToBin(secreta.width, 12);
+    int binWidth = decToBin12(secreta.width);
     printf("altura: ");
-    int binHeight = decToBin(secreta.height, 12);
+    int binHeight = decToBin12(secreta.height);
 
     printf("\nlargura %d, altura %d\n", base.width, base.height);
+    for(int i = 0;i < 2; i ++) {
+        printf("DECIMAL VALUES TO RGB: %d, %d, %d\n", base.img[i].r, base.img[i].g, base.img[i].b);
+        int *binRed = decToBin8(base.img[i].r);
+        int *binGreen= decToBin8(base.img[i].g);
+        int *binBlue = decToBin8(base.img[i].b);
 
-    for(int i = 0;i<)
-        int binRed = decToBin(base.img[i].r, 8);
-        int binGreen = decToBin(base.img[i].g, 8);
-        int binBlue = decToBin(base.img[i].b, 8);
+        printf("RED: \n [");
+        for (int j = 0; j < 8; j ++) {
+            printf("%d", *(binRed + j));
+        }
+        printf("]\n");
 
-        binRed[7] = binWidth[0];
-        binRed[6] = binWidth[1];
-        binGreen[7] = binWidth[2];
-        binGreen[6] = binWidth[3];
-        binBlue[7] = binWidth[4];
-        binBlue[6] = binWidth[5];
+        printf("GREEN: \n [");
+        for (int j = 0; j < 8; j ++) {
+            printf("%d", *(binGreen+ j));
+        }
+        printf("]\n");
+
+        printf("BLUE: \n [");
+        for (int j = 0; j < 8; j ++) {
+            printf("%d", *(binBlue + j));
+        }
+        printf("]\n");
+//        binRed[7] = binWidth[0 + 6 * i];
+//        binRed[6] = binWidth[1 + 6 * i];
+//        binGreen[7] = binWidth[2 + 6 * i];
+//        binGreen[6] = binWidth[3 + 6 * i];
+//        binBlue[7] = binWidth[4 + 6 * i];
+//        binBlue[6] = binWidth[5 + 6 * i];
+//
+//        base.img[i].r = binToDec(*binRed, 8);
+//        base.img[i].g = binToDec(*binGreen, 8);
+//        base.img[i].b = binToDec(*binBlue, 8);
+    }
+
 
 
     //end codigo
 
-//    printf("Primeiros 10 pixels da imagem base:\n");
-//    for(int i=0; i<10; i++)
-//        printf("[%02X %02X %02X] ", base.img[i].r, base.img[i].g, base.img[i].b);
-//    printf("\n\n");
+    printf("Primeiros 2 pixels da imagem base:\n");
+    for(int i=0; i<2; i++)
+        printf("[%02X %02X %02X] ", base.img[i].r, base.img[i].g, base.img[i].b);
+    printf("\n\n");
 //
 //    printf("Primeiros 10 pixels da imagem secreta:\n");
 //    for(int i=0; i<10; i++)
